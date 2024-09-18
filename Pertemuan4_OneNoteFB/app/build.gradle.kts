a5a83a64d30004414091f24d4b920d6153cb9d74
midas79 <dionisius.suryajaya@gmail.com>
2024-09-19 00:07:38 +0700

    Push Pertemuan 4 Fix

--- Pertemuan4_OneNoteFB/app/build.gradle.kts
+++ Pertemuan4_OneNoteFB/app/build.gradle.kts
@@ -61,6 +61,8 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.firebase.auth)
+    implementation(platform("com.google.firebase:firebase-bom:33.3.0"))
+    implementation("com.google.firebase:firebase-analytics")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- Pertemuan4_OneNoteFB/app/src/main/java/com/example/pertemuan4_onenotefb/MainActivity.kt
+++ Pertemuan4_OneNoteFB/app/src/main/java/com/example/pertemuan4_onenotefb/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.auth
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
